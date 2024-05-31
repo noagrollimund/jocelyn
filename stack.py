336df1191a2c45af94a9bcd508c4c48197766aa7
Noa Grollimund <noa.grollimund@icloud.com>
2024-05-31 12:18:56 +0200

    Deconvolution of the stacked data using jocelyn_clean

--- stack.py
+++ stack.py
@@ -1,55 +1,20 @@
 import glob, sys, os
-import config as cfg
+from calibratimage import jocelyn_clean
 
 def main(path_stack):
     os.chdir(path_stack)
-
     list_of_target_ms = glob.glob('../*/Cband/*_target.ms')
     myms = '4U1755_ATCA_Cband.ms'
 
-    from casatasks import concat, tclean
-
+    from casatasks import concat
     concat(vis = list_of_target_ms,
            concatvis = myms)
-
     imagename = myms.replace('.ms', '')
-    # tclean(vis = myms,
-    #        datacolumn = 'data',
-    #        imagename = imagename,
-    #        imsize = cfg.IMSIZE,
-    #        cell = cfg.CELL,
-    #        gridder = cfg.GRIDDER,
-    #        wprojplanes = cfg.WPROJPLANES,
-    #        pblimit = cfg.PBLIMIT,
-    #        deconvolver = cfg.DECONVOLVER,
-    #        scales = cfg.SCALES,
-    #        weighting = cfg.WEIGHTING,
-    #        robust = cfg.ROBUST,
-    #        niter = 10000,
-    #        nsigma = 3.0,
-    #        usemask = cfg.USEMASK,
-    #        pbmask = cfg.PBMASK,
-    #        sidelobethreshold = 4.0,
-    #        noisethreshold = cfg.NOISETHRESHOLD,
-    #        lownoisethreshold = cfg.LOWNOISETHRESHOLD,
-    #        minbeamfrac = cfg.MINBEAMFRAC,
-    #        fastnoise = False,
-    #        parallel = cfg.PARALLEL)
-    tclean(vis = myms,
-           datacolumn = 'data',
-           imagename = imagename,
-           imsize = cfg.IMSIZE,
-           cell = cfg.CELL,
-           gridder = cfg.GRIDDER,
-           wprojplanes = cfg.WPROJPLANES,
-           pblimit = cfg.PBLIMIT,
-           deconvolver = cfg.DECONVOLVER,
-           scales = cfg.SCALES,
-           weighting = cfg.WEIGHTING,
-           robust = cfg.ROBUST,
-           niter = 10000,
-           nsigma = 3,
-           parallel = cfg.PARALLEL)
+    jocelyn_clean(ms = myms,
+                  datacolumn = 'data',
+                  imagename = imagename,
+                  usemask = 'user',
+                  savemodel = 'none')
 
 if __name__ == "__main__":
     main(sys.argv[1])
