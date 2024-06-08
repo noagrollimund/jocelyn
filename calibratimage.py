9205aac3e09515f6d5ee82bdfaf8f44da77d8c9c
Noa Grollimund <noa.grollimund@icloud.com>
2024-06-08 09:54:25 +0200

    Moved tools.py

--- calibratimage.py
+++ calibratimage.py
@@ -1,6 +1,6 @@
 import json, os, sys
 from casatasks import gencal, plotweather, setjy, gaincal, bandpass, fluxscale, applycal, split, tclean, delmod, clearcal, blcal, polcal, statwt
-import tools.tools as tools
+import tools
 import config as cfg
 sys.path.append(os.path.expanduser('~') + '/analysis_scripts/')
 import analysisUtils as au
