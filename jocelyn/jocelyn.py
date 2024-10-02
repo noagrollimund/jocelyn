7541e2c93143c68178e72d8f018cff6628c214af
Noa Grollimund <noa.grollimund@icloud.com>
2024-10-03 01:21:54 +0200

    Option to run without specifying obs path

--- jocelyn/jocelyn.py
+++ jocelyn/jocelyn.py
@@ -1,6 +1,8 @@
 import sys, os
 
 def main(path_obs):
+    if path_obs == '':
+        path_obs = os.getcwd()
     os.chdir(path_obs)
 
     from jocelyn.jocelyn import look
@@ -10,4 +12,5 @@ def main(path_obs):
     calibratimage.main()
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    options = sys.argv[1] if len(sys.argv) > 1 else ''
+    main(options)
