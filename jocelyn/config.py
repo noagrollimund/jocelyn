2dda38c88f750a3bdd38c1b1354ce7197a79e788
Noa Grollimund <noa.grollimund@icloud.com>
2025-03-25 18:16:15 +0100

    TABLES and IMAGES directory name

--- jocelyn/config.py
+++ jocelyn/config.py
@@ -12,10 +12,10 @@ BAND = 'C'
 #----- Paths ---------------------------------------------#
 PATH_OBS = os.getcwd()
 PATH_CODE = PATH_OBS + '/jocelyn/'
-PATH_BAND = PATH_OBS + f'/{BAND}band/'
+PATH_BAND = PATH_OBS + f'/{BAND}-band/'
 PATH_JSON = PATH_BAND + 'info.json'
-PATH_TABLES = PATH_BAND + 'tables/'
-PATH_IMAGES = PATH_BAND + 'images/'
+PATH_TABLES = PATH_BAND + 'TABLES/'
+PATH_IMAGES = PATH_BAND + 'IMAGES/'
 
 
 #----- Flagging ------------------------------------------#
