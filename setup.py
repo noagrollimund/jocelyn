3e0a6ab8ee27fd47afe0d21426423e7666a4894b
Noa Grollimund <noa.grollimund@icloud.com>
2025-03-25 19:03:05 +0100

    Fixed kill file scancel instruction

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def main():
     kill_file = cfg.PATH_SCRIPTS + 'kill_jobs.sh'
     instruct = '#!/bin/bash\n'
     instruct += f"JOCELYN=`sbatch {slurm_jocelyn}" + " | awk '{print $4}'`\n"
-    instruct += f"echo 'scancel $JOCELYN' > {kill_file}"
+    instruct += f"echo 'scancel '$JOCELYN > {kill_file}"
     gen.write_slurm(submit_file, instruct)
 
 if __name__ == "__main__":
