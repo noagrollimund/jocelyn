0e6f69a38f8927ef5386aacd1703146c940ea790
Noa Grollimund <noa.grollimund@icloud.com>
2025-03-25 17:53:18 +0100

    Removed STACK, simplified setup

--- jocelyn/generate_jobs.py
+++ jocelyn/generate_jobs.py
@@ -6,7 +6,7 @@ def write_slurm(slurm_file, instruct):
         f.write(instruct)
     os.system('chmod +x ' + slurm_file)
 
-def slurm_jocelyn(path_obs, path_code):
+def slurm_jocelyn(path, path_code):
     instruct = "#!/bin/bash\n"
     instruct += "#SBATCH --job-name=JOCELYN\n"
     instruct += "#SBATCH --time=4:00:00\n"
@@ -15,44 +15,20 @@ def slurm_jocelyn(path_obs, path_code):
     instruct += "#SBATCH --ntasks-per-node=8\n"
     instruct += "#SBATCH --cpus-per-task=1\n"
     instruct += "#SBATCH --mem=64GB\n"
-    instruct += f"#SBATCH --output={path_obs}JOCELYN.log\n"
-    instruct += f"cd {path_obs}\n"
+    instruct += f"#SBATCH --output={path}/JOCELYN.log\n"
+    instruct += f"cd {path}\n"
     instruct += "module load openmpi/4.0.3\n"
     instruct += f"singularity exec {cfg.CASA_CONTAINER_PATH} python {path_code}0_INFO.py\n"
     instruct += f"mpirun singularity exec {cfg.CASA_CONTAINER_PATH} python {path_code}1GC.py\n"
     instruct += f"mpirun singularity exec {cfg.CASA_CONTAINER_PATH} python {path_code}2GC.py\n"
     instruct += 'echo "****ELAPSED "$SECONDS""\n'
-    slurm_file = f'{path_obs}JOCELYN.sh'
+    slurm_file = f'{path}/JOCELYN.sh'
     write_slurm(slurm_file, instruct)
 
-def slurm_stack(path_stack, path_code):
-    instruct = "#!/bin/bash\n"
-    instruct += "#SBATCH --job-name=STACK\n"
-    instruct += "#SBATCH --time=15:00:00\n"
-    instruct += "#SBATCH --partition=Main\n"
-    instruct += "#SBATCH --nodes=1\n"
-    instruct += "#SBATCH --ntasks-per-node=8\n"
-    instruct += "#SBATCH --cpus-per-task=1\n"
-    instruct += "#SBATCH --mem=64GB\n"
-    instruct += f"#SBATCH --output={path_stack}STACK.log\n"
-    instruct += f"cd {path_stack}\n"
-    instruct += "module load openmpi/4.0.3\n"
-    instruct += f"mpirun singularity exec {cfg.CASA_CONTAINER_PATH} python {path_code}STACK.py\n"
-    instruct += 'echo "****ELAPSED "$SECONDS""\n'
-    slurm_file = f'{path_stack}STACK.sh'
-    write_slurm(slurm_file, instruct)
-
-def slurm_main(path, paths_obs, path_stack):
-    instruct = "#!/bin/bash\n"
-    for i, path_obs in enumerate(paths_obs):
-        instruct += f"jcl{i}=`sbatch {path_obs}JOCELYN.sh" + " | awk '{print $4}'`\n"
-    list_of_jobs = [f"jcl{i}" for i in range(len(paths_obs))]
-    if path_stack != None:
-        afterok = ''.join([':$' + name for name in list_of_jobs])
-        instruct += f"STACK=`sbatch --dependency=afterok{afterok} {path_stack}STACK.sh" + " | awk '{print $4}'`\n"
-        list_of_jobs.append('STACK')
-    scancel_jobs = ' '.join(['$' + name for name in list_of_jobs])
+def slurm_main(path):
     slurm_file = path + '/submit_jobs.sh'
     kill_file = path + "/kill_jobs.sh"
-    instruct += f"echo 'scancel '{scancel_jobs} > {kill_file}"
+    instruct = "#!/bin/bash\n"
+    instruct += f"JOCELYN=`sbatch {path}JOCELYN.sh" + " | awk '{print $4}'`\n"
+    instruct += f"echo 'scancel JOCELYN' > {kill_file}"
     write_slurm(slurm_file, instruct)
--- setup.py
+++ setup.py
@@ -1,36 +1,9 @@
-import os, glob
 import jocelyn.config as cfg
 import jocelyn.generate_jobs as gen
 
-def symlinks(path_archive, date):
-    archives = glob.glob(path_archive + date + '*.C*')
-    for archive in archives:
-        link_archive = archive + ' ' + date
-        os.system('ln -s ' + link_archive)
-    flags_file = glob.glob(path_archive + date + '*.flag')
-    link_flags = flags_file[0] + ' ' + date
-    os.system('ln -s ' + link_flags)
-
 def main():
-    path = os.getcwd()
-    path_archive = path + '/archive/'
-    path_code = path + '/jocelyn/'
-    path_stack = path + '/stack/' if cfg.STACK_OBS else None
-    paths_obs = []
-    if not os.path.exists(path_archive):
-        raise FileNotFoundError('No archive directory found.')
-    for date in cfg.DATES:
-        path_obs = f'{path}/{date}/'
-        if not os.path.exists(path_obs):
-            os.makedirs(path_obs)
-        symlinks(path_archive, date)
-        gen.slurm_jocelyn(path_obs, path_code)
-        paths_obs.append(path_obs)
-    if cfg.STACK_OBS:
-        if not os.path.exists(path_stack):
-            os.makedirs(path_stack)
-        gen.slurm_stack(path_stack, path_code)
-    gen.slurm_main(path, paths_obs, path_stack)
+    gen.slurm_jocelyn(cfg.PATH_OBS, cfg.PATH_CODE)
+    gen.slurm_main(cfg.PATH_OBS)
 
 if __name__ == "__main__":
     main()
